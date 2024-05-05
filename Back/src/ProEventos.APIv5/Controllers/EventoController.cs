7daa8d7e3ad8f09e794aa727b8746cdc6592e521
Jon.Ocx <joaoalmeida48@outlook.com>
2024-05-05 02:28:54 -0300

    Adicionado eventos Controllers

--- Back/src/ProEventos.APIv5/Controllers/EventoController.cs
+++ Back/src/ProEventos.APIv5/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProEventos.APIv5.Data;
 using ProEventos.APIv5.Models;
 
 namespace ProEventos.APIv5.Controllers
@@ -12,36 +13,11 @@ namespace ProEventos.APIv5.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<EventoController> _logger;
-        public  IEnumerable<Eventos> _eventos = new Eventos[]
-        {
-                new Eventos(){
-                EventoId = 1,
-                Tema = "Angular 11 e .NET 5",
-                Local = "São Paulo",
-                Lote = "1 Lote",
-                QtdPessoas = 258,
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                ImagemURL = "Foto.png"
-            },
-                new Eventos(){
-                EventoId = 2,
-                Tema = "Angular e suas novidades",
-                Local = "São Paulo",
-                Lote = "1 Lote",
-                QtdPessoas = 238,
-                DataEvento = DateTime.Now.AddDays(2).ToString(),
-                ImagemURL = "AltereiFoto.png"
-            }
-        };
+        private readonly DataContext _context;
         
-        public EventoController()
+        public EventoController(DataContext context)
         {
+            this._context = context;
            
         }
 
@@ -49,14 +25,14 @@ namespace ProEventos.APIv5.Controllers
         [HttpGet]
         public IEnumerable<Eventos> Get()
         {
-            return _eventos;
+            return _context.Eventos;
         }
 
         // retorna evento com base no ID
         [HttpGet("{id}")]
         public IEnumerable<Eventos> Get(int id)
         {
-            return _eventos.Where(e => e.EventoId == id);
+            return  _context.Eventos.Where(e => e.EventoId == id);
         }
 
         [HttpPost]
